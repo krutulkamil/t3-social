41b936f029061e7fbcbf1d0be7455939e2de05e5
krutulkamil <krutulkamil@onet.pl>
2024-02-03 16:25:53 +0100

    feat: prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,10 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
   plugins: ["prettier-plugin-tailwindcss"],
 };
 

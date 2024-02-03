3e11925da56400ceb306a5f3d51fb40511d2a8ed
krutulkamil <krutulkamil@onet.pl>
2024-02-03 17:17:21 +0100

    feat: infiniteFeed procedure select, next cursor logic

--- src/server/api/routers/tweet.ts
+++ src/server/api/routers/tweet.ts
@@ -16,11 +16,42 @@ export const tweetRouter = createTRPCRouter({
       }),
     )
     .query(async ({ input: { limit = 10, cursor }, ctx }) => {
-      return ctx.db.tweet.findMany({
+      const currentUserId = ctx.session?.user.id;
+
+      const data = await ctx.db.tweet.findMany({
         take: limit + 1,
         orderBy: [{ createdAt: 'desc' }, { id: 'desc' }],
         cursor: cursor ? { createdAt_id: cursor } : undefined,
+        select: {
+          id: true,
+          content: true,
+          createdAt: true,
+          _count: { select: { likes: true } },
+          likes: currentUserId ? { where: { userId: currentUserId } } : false,
+          user: { select: { name: true, id: true, image: true } },
+        },
       });
+
+      let nextCursor: typeof cursor | undefined;
+      if (data.length > limit) {
+        const nextItem = data.pop();
+        if (nextItem) {
+          nextCursor = { id: nextItem.id, createdAt: nextItem.createdAt };
+        }
+      }
+
+      const tweets = data.map((tweet) => {
+        return {
+          id: tweet.id,
+          content: tweet.content,
+          createdAt: tweet.createdAt,
+          likeCount: tweet._count.likes,
+          user: tweet.user,
+          likedByMe: tweet.likes?.length > 0,
+        };
+      });
+
+      return { tweets, nextCursor };
     }),
 
   create: protectedProcedure.input(z.object({ content: z.string() })).mutation(async ({ input: { content }, ctx }) => {

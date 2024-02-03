924fdb45c214e55107ea3f61aedc350b1a31abc0
krutulkamil <krutulkamil@onet.pl>
2024-02-03 16:40:46 +0100

    feat: tweet router /w create tweet mutation

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,7 +1,8 @@
-import { type ChangeEvent, useCallback, useLayoutEffect, useRef, useState } from "react";
+import React, { useCallback, useLayoutEffect, useRef, useState, type ChangeEvent } from "react";
 import { updateTextAreaSize } from "~/utils/updateTextAreaSize";
 import { ProfileImage } from "~/components/ProfileImage";
 import { Button } from "~/components/Button";
+import { api } from '~/utils/api';
 import type { SessionContextValue } from "next-auth/react";
 import type { Session } from "next-auth";
 
@@ -25,12 +26,24 @@ export function NewTweetForm({ sessionStatus, sessionData }: NewTweetFormProps) 
 
   if (sessionStatus !== "authenticated") return;
 
+  const createTweet = api.tweet.create.useMutation({
+    onSuccess: (newTweet) => {
+      console.log("New tweet created:", newTweet);
+      setInputValue("");
+    },
+  });
+
   function handleTextAreaValue(event: ChangeEvent<HTMLTextAreaElement>) {
     return setInputValue(event.target.value);
   }
 
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    createTweet.mutate({ content: inputValue });
+  }
+
   return (
-    <form className="flex flex-col gap-2 border-b px-4 py-2">
+    <form onSubmit={handleSubmit} className="flex flex-col gap-2 border-b px-4 py-2">
       <div className="flex gap-4">
         <ProfileImage src={sessionData.user.image} />
         <textarea
---
+++ src/server/api/routers/tweet.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+import { createTRPCRouter, protectedProcedure } from '~/server/api/trpc';
+
+export const tweetRouter = createTRPCRouter({
+  create: protectedProcedure
+    .input(z.object({ content: z.string() }))
+    .mutation(async ({ input: { content }, ctx }) => {
+      return ctx.db.tweet.create({
+        data: { content, userId: ctx.session.user.id }
+      });
+  }),
+});

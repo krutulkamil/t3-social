9a048052bce4ff70b1b517d53636bb8e0174378c
krutulkamil <krutulkamil@onet.pl>
2024-02-04 19:00:30 +0100

    feat: init ssgHelper

--- src/components/InfiniteTweetList.tsx
+++ src/components/InfiniteTweetList.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import { TweetCard } from '~/components/TweetCard';
+import { LoadingSpinner } from '~/components/LoadingSpinner';
 import type { Tweet } from '~/types/tweet';
 
 interface InfiniteTweetListProps {
@@ -12,7 +13,7 @@ interface InfiniteTweetListProps {
 }
 
 export function InfiniteTweetList({ tweets, isError, isLoading, hasMore = false, fetchNewTweets }: InfiniteTweetListProps) {
-  if (isLoading) return <h1>Loading...</h1>;
+  if (isLoading) return <LoadingSpinner />;
   if (isError) return <h1>Error...</h1>;
   if (!tweets || tweets.length === 0) {
     return <h2 className="my-4 text-center text-2xl text-gray-500">No Tweets</h2>;
@@ -20,7 +21,7 @@ export function InfiniteTweetList({ tweets, isError, isLoading, hasMore = false,
 
   return (
     <ul>
-      <InfiniteScroll next={fetchNewTweets} hasMore={hasMore} loader="Loading..." dataLength={tweets.length}>
+      <InfiniteScroll next={fetchNewTweets} hasMore={hasMore} loader={<LoadingSpinner />} dataLength={tweets.length}>
         {tweets.map((tweet) => (
           <TweetCard key={tweet.id} {...tweet} />
         ))}

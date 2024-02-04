b15e533587787ae14d2238b27eea92ebe37ce332
krutulkamil <krutulkamil@onet.pl>
2024-02-04 18:16:14 +0100

    feat: main page tabs component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,41 @@
+import React, { useState } from 'react';
+import { useSession } from 'next-auth/react';
+
+import { NewTweetFormWrapper } from '~/components/NewTweetFormWrapper';
+import { RecentTweets } from '~/components/RecentTweets';
+
+const TABS = ['Recent', 'Following'] as const;
+
 export default function Home() {
-  return <h1>hi</h1>
+  const [selectedTab, setSelectedTab] = useState<(typeof TABS)[number]>('Recent');
+
+  const session = useSession();
+
+  return (
+    <>
+      <header className="sticky top-0 z-10 border-b bg-white pt-2">
+        <h1 className="mb-2 px-4 text-lg font-bold">Home</h1>
+        {session.status === 'authenticated' && (
+          <div className="flex">
+            {TABS.map((tab) => {
+              return (
+                <button
+                  key={tab}
+                  className={`flex-grow p-2 hover:bg-gray-200 focus-visible:bg-gray-200 ${
+                    tab === selectedTab ? 'border-b-4 border-b-blue-500 font-bold' : ''
+                  }`}
+                  onClick={() => setSelectedTab(tab)}
+                >
+                  {tab}
+                </button>
+              );
+            })}
+          </div>
+        )}
+      </header>
+
+      <NewTweetFormWrapper />
+      <RecentTweets />
+    </>
+  );
 }

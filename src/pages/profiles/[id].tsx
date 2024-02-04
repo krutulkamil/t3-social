2a5a80dd065cde9893ebef60b6a724e79ca8f60a
krutulkamil <krutulkamil@onet.pl>
2024-02-04 19:34:06 +0100

    feat: profile page header component /w follows count

--- src/pages/profiles/[id].tsx
+++ src/pages/profiles/[id].tsx
@@ -1,8 +1,13 @@
 import Head from 'next/head';
-import type { GetStaticPaths, GetStaticPropsContext, InferGetStaticPropsType, NextPage } from 'next';
+import Link from 'next/link';
 import ErrorPage from 'next/error';
-import { ssgHelper } from '~/server/api/ssgHelper';
+import type { GetStaticPaths, GetStaticPropsContext, InferGetStaticPropsType, NextPage } from 'next';
+import { VscArrowLeft } from 'react-icons/vsc';
+import { IconHoverEffect } from '~/components/IconHoverEffect';
+import { ProfileImage } from '~/components/ProfileImage';
 import { api } from '~/utils/api';
+import { ssgHelper } from '~/server/api/ssgHelper';
+import { getPlural } from '~/utils/getPlural';
 
 const ProfilePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ id }) => {
   const { data: profile } = api.profile.getById.useQuery({ id });
@@ -12,9 +17,23 @@ const ProfilePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
   return (
     <>
       <Head>
-        <title>{`Twitter Clone ${profile.name}`}</title>
+        <title>{`Twitter Clone - ${profile.name}`}</title>
       </Head>
-      {profile.name}
+      <header className="sticky top-0 z-10 flex items-center border-b bg-white px-4 py-2">
+        <Link href=".." className="mr-2">
+          <IconHoverEffect>
+            <VscArrowLeft className="h-6 w-6" />
+          </IconHoverEffect>
+        </Link>
+        <ProfileImage src={profile.image} className="flex-shrink-0" />
+        <div className="ml-2 flex-grow">
+          <h1 className="text-lg font-bold">{profile.name}</h1>
+          <div className="text-gray-500">
+            {profile.tweetsCount} {getPlural(profile.tweetsCount, 'Tweet', 'Tweets')} - {profile.followersCount}{' '}
+            {getPlural(profile.followersCount, 'Follower', 'Followers')} - {profile.followsCount} Following
+          </div>
+        </div>
+      </header>
     </>
   );
 };

b9ca56412a2920a9b68d2a59f80a25edc3c5105a
krutulkamil <krutulkamil@onet.pl>
2024-02-04 19:49:52 +0100

    feat: infiniteProfileFeed public procedure

--- src/server/api/routers/tweet.ts
+++ src/server/api/routers/tweet.ts
@@ -5,6 +5,28 @@ import { type inferAsyncReturnType } from '@trpc/server';
 import { type createTRPCContext, createTRPCRouter, protectedProcedure, publicProcedure } from '~/server/api/trpc';
 
 export const tweetRouter = createTRPCRouter({
+  infiniteProfileFeed: publicProcedure
+    .input(
+      z.object({
+        userId: z.string(),
+        limit: z.number().optional(),
+        cursor: z
+          .object({
+            id: z.string(),
+            createdAt: z.date(),
+          })
+          .optional(),
+      }),
+    )
+    .query(async ({ input: { limit = 10, userId, cursor }, ctx }) => {
+      return await getInfiniteTweets({
+        limit,
+        ctx,
+        cursor,
+        whereClause: { userId },
+      });
+    }),
+
   infiniteFeed: publicProcedure
     .input(
       z.object({
